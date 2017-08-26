ccc410b5c67d003c47b1e7ea63d242b75adcede2
iampratikshinde <pratikshinde320@gmail.com>
2017-08-27 01:16:56 +0530

    Now supports DHT.

--- src/main/java/dht/DHTConnection.java
+++ src/main/java/dht/DHTConnection.java
@@ -5,8 +5,11 @@ import bencodeutils.Element;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.*;
 import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -14,28 +17,34 @@ import java.util.TreeMap;
  * Created by ps on 7/6/17.
  */
 public class DHTConnection {
+    public static void main(String[] args) throws IOException {
+        DHTConnection connection=new DHTConnection();
+        connection.connect();
+    }
     String[] dhtBootStrapNodes={"router.utorrent.com","router.bittorrent.com"};
     public void connect() throws IOException {
-        Map<String, Element> pingRequest=new TreeMap<String, Element>();
-        pingRequest.put("t",new Element("0"));
-        pingRequest.put("y",new Element("q"));
-        pingRequest.put("q",new Element("ping"));
-        Map<String,Element> pingID=new TreeMap<String, Element>();
-        pingID.put("id",new Element("pratiks123"));
-        pingRequest.put("a",new Element(pingID));
-
+        Map<String,Element> queryRequest=new HashMap<String, Element>();
+        Map<String,Element> queryParam=new HashMap<String, Element>();
+        queryParam.put("id",new Element(new String("abcdefghij0123456789")));
+        queryParam.put("info_hash",new Element(new String("f2e59d5609a5e3bb80995547e404fc95f799909e")));
+        queryRequest.put("a",new Element(queryParam));
+        queryRequest.put("q",new Element(new String("get_peers")));
+        queryRequest.put("t",new Element(new String("aa")));
+        queryRequest.put("y",new Element(new String("q")));
         ByteArrayOutputStream stream=new ByteArrayOutputStream();
-        BencodeUtils.encode(pingRequest,stream);
-        byte data[]=send(stream.toByteArray());
-        if(data==null){
-            System.out.println("Nothing returned.");
-        }
-        System.out.println("length "+data.length);
-        Element element=BencodeUtils.decode(data);
-        Map<String,Element> root=element.getMap();
-        Map<String,Element> idKey=root.get("r").getMap();
-        String id=idKey.get("id").getString();
-        System.out.println("Remote id is :"+id);
+        BencodeUtils.encode(queryRequest,stream);
+        byte output[]=send(stream.toByteArray());
+        Element element=BencodeUtils.decode(output);
+        Map<String,Element> response=element.getMap();
+        Map<String,Element> map=response.get("r").getMap();
+        System.out.println("ID is "+ map.get("id").getString());
+        byte value[]=map.get("nodes").getBytes();
+        byte ip[]=new byte[6];
+        System.arraycopy(value,0,ip,0,6);
+        ByteBuffer byteBuffer=ByteBuffer.wrap(ip);
+        InetSocketAddress address;
+        address=getInetSocketAddress(byteBuffer,0,4);
+        System.out.println(address);
 
     }
     public byte[] send( byte data[]){
@@ -60,5 +69,16 @@ public class DHTConnection {
             e.printStackTrace();
         }
         return null;
+    }
+    private InetSocketAddress getInetSocketAddress(ByteBuffer buffer,int offset,int size){
+        byte ip_address[]=ByteBuffer.allocate(size).putInt(buffer.getInt(offset)).array();
+        int port= buffer.getShort(offset+size) & 0xFFFF;
+        InetSocketAddress address=null;
+        try {
+            address=new InetSocketAddress(InetAddress.getByAddress(ip_address),port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return address;
     }
 }
